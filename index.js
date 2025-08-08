1e2abcb7f087647c7b7b4885f350a648f2070944
Neal Ning <yichen.ning.2021@gmail.com>
2025-08-08 14:54:50 +0200

    Added js logic for animation and navigation from index page.

---
+++ index.js
@@ -0,0 +1,51 @@
+// List of menu items
+const menu = [
+    {
+        text: " About Me",
+        sc: "a",
+        dest: "./aboutMe.html"
+    },
+    {
+        text: "󰛨 Projects",
+        sc: "p",
+        dest: "./projects.html"
+    },
+    {
+        text: "󰋄 Music",
+        sc: "m",
+        dest: "./music.html"
+    },
+]
+
+// Select the button container
+const buttons = document.querySelector(".db-buttons");
+
+menu.forEach(({text, sc, dest}) => {
+    // Create div to contain text, shortcut key text, and some data
+    const menuItemDiv = document.createElement("div");
+    menuItemDiv.classList.add("db-button");
+    menuItemDiv.dataset.sc = sc;
+    menuItemDiv.dataset.dest = dest;
+    menuItemDiv.innerHTML = `<span>${text}</span><span>${sc}</span>`;
+
+    // Clicking redirects to page whose path is stored in ${dest}
+    menuItemDiv.addEventListener("click", () => {
+        window.location.href = dest;
+    });
+
+    // Render the buttons
+    buttons.appendChild(menuItemDiv);
+});
+
+// Listens for pressing of the short cut keys of each menu item
+document.body.addEventListener("keydown", (event) => {
+    const btn = document.querySelector(`.db-button[data-sc="${event.key.toLowerCase()}"]`);
+    if (btn) btn.classList.add("db-button-select");
+});
+document.body.addEventListener("keyup", (event) => {
+    const btn = document.querySelector(`.db-button[data-sc="${event.key.toLowerCase()}"]`);
+    if (btn) {
+        btn.classList.remove("db-button-select");
+        window.location.href = btn.dataset.dest; 
+    }
+});
