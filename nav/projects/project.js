98efabb3352a72522dce2621130d03c25262014e
Neal Ning <yichen.ning.2021@gmail.com>
2025-08-09 22:03:20 +0200

    Added correct titles, and dynamic titles for projects

--- nav/projects/project.js
+++ nav/projects/project.js
@@ -7,19 +7,16 @@ utils.createBackButton(backButtonContainer, "/nav/projects/projects.html");
 // Add keydown / keyup listeners for buttons
 utils.addButtonKeyListeners();
 
-// Query and find the data of project specified in the url params
-function getSingleProjectData(projectData) {
-    const params = new URLSearchParams(window.location.search);
-    const projectName = params.get("name");
-    return projectData.find(data => data.name === projectName);
-}
+// Name of project specified in the url params
+const projectName = new URLSearchParams(window.location.search).get("name");
+document.querySelector("title").innerText = projectName;
 
 // Fetch and load data corresponding to the requested project
 const container = document.querySelector(".flex-grow-container");
 (async () => {
     try {
         const projectData = await utils.loadData("projectData.json")
-        const singleProjectData = getSingleProjectData(projectData);
+        const singleProjectData = projectData.find(data => data.name === projectName);
         utils.displayData(container, singleProjectData);
     } catch (err) {
         console.log(`Error: ${err}`);
